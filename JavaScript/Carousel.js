fa2d0e4df2aea881a8ee04fe7a21b8eac3b20e8e
Paulo Baier <baierpaulo312@gmail.com>
2025-10-22 23:17:27 -0300

    Slider Página Inicial Feito

--- JavaScript/Carousel.js
+++ JavaScript/Carousel.js
@@ -1,22 +1,33 @@
 const carousel = document.querySelectorAll('.carousel-item');
 let index = 0;
 
-function atualizarPosicaocarousel(){
+function atualizarPosicaocarousel(direction){
     carousel.forEach((item) => item.classList.remove("active", "next", "prev"));
     carousel[index].classList.add("active");
     carousel[(index + 1) % carousel.length].classList.add("next");
     carousel[(index - 1 + carousel.length) % carousel.length].classList.add("prev");
-    
+
+    if(direction == "prev"){
+        carousel[(index + 1) % carousel.length].style.zIndex = "1";
+        carousel[(index - 1 + carousel.length) % carousel.length].style.zIndex = "0";
+        carousel[index].style.zIndex = "2";
+    }
+    if(direction == "next"){
+        carousel[(index + 1) % carousel.length].style.zIndex = "0";
+        carousel[(index - 1 + carousel.length) % carousel.length].style.zIndex = "1";
+        carousel[index].style.zIndex = "2";
+    }
 }
 
 document.getElementById("btn-next").onclick = () => {
     index = (index + 1) % carousel.length;
-    atualizarPosicaocarousel();
+    atualizarPosicaocarousel("next");
 }
 
 document.getElementById("btn-prev").onclick = () => {
     index = (index - 1 + carousel.length) % carousel.length;
-    atualizarPosicaocarousel();
+
+    atualizarPosicaocarousel('prev');
 }
 
 atualizarPosicaocarousel();
